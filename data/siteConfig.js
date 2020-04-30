2994396381c794ed842498cee3d250015dc9f127
carlos.mederos@tanner.cl <carlos.mederos@tanner.cl>
2020-04-29 23:17:59 -0400

    Initial commit from gatsby: (https://github.com/ajimenezdev/gatsby-cv.git)

---
+++ data/siteConfig.js
@@ -0,0 +1,268 @@
+module.exports = {
+  siteTitle: "Hi! I'm Alvaro!",
+  siteDescription: `This is my personal website`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Alvaro Jimenez',
+  twitterUsername: 'ajimenezdev',
+  githubUsername: 'ajimenezdev',
+  authorAvatar: '/images/avatar.jpg',
+  authorDescription: `I'm a full-stack JS developer, currently focused in React/React Native development and blockchain solutions.<br/><br/>
+  I worked in multiple stacks (React, AngularJs, .Net, Java, Android...), environments and types of companies (Startup, medium size and big corporate).<br/><br/>
+  I currently work remotely as a frontend developer and mainly working with <strong>Javascript, React/ReactNative, NodeJS and Blockchain.</strong>`,
+  skills: [
+    {
+      name: 'HTML',
+      level: 90,
+    },
+    {
+      name: 'CSS',
+      level: 85,
+    },
+    {
+      name: 'Javascript',
+      level: 90,
+    },
+    {
+      name: 'React',
+      level: 85,
+    },
+    {
+      name: 'ReactNative',
+      level: 80,
+    },
+    {
+      name: 'NodeJs',
+      level: 40,
+    },
+    {
+      name: 'Git',
+      level: 75,
+    },
+    {
+      name: 'Bitcoin',
+      level: 30,
+    },
+    {
+      name: 'Ethereum & smartcontract',
+      level: 30,
+    },
+  ],
+  jobs: [
+    {
+      company: 'Bitfinex',
+      begin: {
+        month: 'oct',
+        year: '2019',
+      },
+      duration: null,
+      location: 'Remote',
+      occupation: 'Mobile Engineer',
+      description:
+        'Working mainly with ReactNative as part of the Mobile development team, handling any task related to improve or fix existing features and developing new ones.',
+    },
+    {
+      company: 'Lifelabs.io',
+      begin: {
+        month: 'may',
+        year: '2018',
+      },
+      duration: '1 years and 5 months',
+      location: 'UK - Remote',
+      occupation: 'Lead Frontend Developer',
+      description:
+        'I joined the company as one of the main developers, leading the frontend and mobile app development and helping with the architecture and coordination of the backend and blokchain teams.',
+    },
+    {
+      company: 'Flipper',
+      begin: {
+        month: 'oct',
+        year: '2015',
+      },
+      duration: '2 years and 7 months',
+      location: 'London, UK',
+      occupation: 'Senior Developer',
+      description:
+        'I joined the company as one of the first 5 employees, I have been involved in all technical areas (backend, frontend, db, devops, bi) but with a higher responsibility on frontend, which I was leading inside the team',
+    },
+    {
+      company: 'Radisson Blu Edwardian London',
+      begin: {
+        month: 'oct',
+        year: '2013',
+      },
+      duration: '2 years',
+      location: 'London, UK',
+      occupation: 'Software developer',
+      description:
+        'Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool.',
+    },
+    {
+      company: 'HotelBeds',
+      begin: {
+        month: 'oct',
+        year: '2012',
+      },
+      duration: '1 year',
+      location: 'Majorca, Spain',
+      occupation: 'Software developer',
+      description:
+        'Member of a team in charge to create a new website to sell complementary holidays tickets and excursions, integrated inside the existing booking engine.',
+    },
+    {
+      company: 'Brujula',
+      begin: {
+        month: 'feb',
+        year: '2012',
+      },
+      duration: '7 months',
+      location: 'Majorca, Spain',
+      occupation: 'Junior developer',
+      description:
+        'Working in a team for a third party company as a development house, in charge of their intranet site to manage all the information about new customers, employees, keep the stock up to date and some other small applications.',
+    },
+    {
+      company: 'Bizzit',
+      begin: {
+        month: 'jul',
+        year: '2010',
+      },
+      duration: '3 months',
+      location: 'Majorca, Spain',
+      occupation: 'Intern developer',
+      description:
+        'Summer internship, Working with the company intranet site, creating new features and fixing some of the existing bugs.',
+    },
+    {
+      company: 'Bizzit',
+      begin: {
+        month: 'jul',
+        year: '2009',
+      },
+      duration: '3 months',
+      location: 'Majorca, Spain',
+      occupation: 'Intern developer',
+      description:
+        'Summer internship, Working with the company intranet site, creating new features and fixing some of the existing bugs.',
+    },
+  ],
+  publications: [
+    {
+      title: 'React Native intermediate level',
+      company_medium: 'OpenWebinars',
+      date: {
+        month: 'jun',
+        year: '2019',
+      },
+      link: 'https://openwebinars.net/cursos/react-native-intermedio/',
+      description:
+        'Online training recorded for spanish e-learning platform. The training is for developers who know a little bit of ReactNative and wants to learn more about the platform',
+    },
+    {
+      title: 'React Native for beginners',
+      company_medium: 'OpenWebinars',
+      date: {
+        month: 'jun',
+        year: '2019',
+      },
+      link: 'https://openwebinars.net/cursos/react-native-principiantes/',
+      description:
+        'Online training recorded for spanish e-learning platform. The training is for JS developers willing to learn the basics of ReactNative',
+    },
+  ],
+  education: [
+    {
+      school: 'University of Balearic Islands',
+      degree: 'Bachelor degree',
+      field: 'Software Engineering',
+      startYear: 2006,
+      endYear: 2011,
+    },
+  ],
+  languages: [
+    {
+      language: 'Spanish',
+      level: 'Native tongue',
+      code: 'ES',
+    },
+    {
+      language: 'Catalan',
+      level: 'Native tongue',
+    },
+    {
+      language: 'English',
+      level: 'Fluid Written & Spoken',
+      code: 'GB',
+    },
+  ],
+  social: {
+    twitter: 'https://twitter.com/ajimenezdev/',
+    linkedin: 'https://www.linkedin.com/in/alvarojimenezmartin/',
+    github: 'https://github.com/ajimenezdev/',
+    email: 'ajmjimens@gmail.com',
+  },
+  hobbies: [
+    {
+      name: 'Football',
+      icon: 'FaFutbol',
+    },
+    {
+      name: 'Photography',
+      icon: 'FaCamera',
+    },
+    {
+      name: 'Hiking',
+      icon: 'FaHiking',
+    },
+    {
+      name: 'Traveling',
+      icon: 'FaPlane',
+    },
+    {
+      name: 'Movies & Series',
+      icon: 'FaTv',
+    },
+  ],
+  siteUrl: 'https://alvarojimenezmartin.com',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpeg',
+  googleAnalyticsId: 'UA-4492763-3',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/favicon.jpg',
+  resume: '/resume_alvaro_jimenez.pdf',
+  headerLinks: [
+    // {
+    //   label: 'Alvaro Jimenez Martin',
+    //   url: '/',
+    // },
+    {
+      label: 'About',
+      url: 'about',
+    },
+    {
+      label: 'Skills',
+      url: 'skills',
+    },
+    {
+      label: 'Experience',
+      url: 'experience',
+    },
+    {
+      label: 'Publications',
+      url: 'publications',
+    },
+    {
+      label: 'Education',
+      url: 'education',
+    },
+    {
+      label: 'Hobbies',
+      url: 'hobbies',
+    },
+    // {
+    //   label: 'Contact',
+    //   url: 'contact',
+    // },
+  ],
+}
